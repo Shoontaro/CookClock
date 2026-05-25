fd5460a0e846d885f2ad9c80140d96598ff94942
Yumo <tishinadw@gmail.com>
2026-05-26 02:36:25 +0300

    Добавьте файлы проекта.

---
+++ CookClock/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CookClock
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CookClock/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CookClock
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
