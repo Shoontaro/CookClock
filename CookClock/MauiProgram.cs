b0835971ce638ff8b2ab3b4f2115f614a873edce
Yumo <tishinadw@gmail.com>
2026-05-26 11:53:25 +0300

    - add progress circle - add timer code - change status bar

--- CookClock/MauiProgram.cs
+++ CookClock/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace CookClock
 {
@@ -14,13 +18,42 @@ namespace CookClock
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            builder
+           .UseMauiApp<App>()
+           .ConfigureLifecycleEvents(events =>
+           {
+#if ANDROID
+               events.AddAndroid(android =>
+               {
+                   android.OnCreate((activity, bundle) =>
+                   {
+                       var window = activity.Window;
+
+                       if (window is null)
+                           return;
+
+                       window.SetStatusBarColor(
+                           Android.Graphics.Color.ParseColor("#FFB703"));
+
+                       window.SetNavigationBarColor(
+                           Android.Graphics.Color.ParseColor("#FFB703"));
+                   });
+               });
+#endif
+           });
+
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
-
+            builder.Services.AddMudServices();
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass =
+                    Defaults.Classes.Position.BottomCenter;
+            });
             return builder.Build();
         }
     }
